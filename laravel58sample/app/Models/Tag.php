745801b7e730b62a161e4f2bf45487629cecfdeb
DominikStyp <dominik.stypula1@gmail.com>
2019-08-19 23:52:32 +0200

    Models relations

--- laravel58sample/app/Models/Tag.php
+++ laravel58sample/app/Models/Tag.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     public $table = 'tags';
+
+
+
+    public function categories()
+    {
+        return $this->morphedByMany(Category::class, 'taggable');
+    }
+
+
 }
